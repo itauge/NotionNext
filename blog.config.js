2a6c3b9d9289095f793850d31803332c964f238c
itauge <itaugee@gmail.com>
2022-05-29 14:50:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
   AUTHOR: 'itauge', // 作者
-  BIO: 'Be a Man', // 作者简介
+  BIO: 'Be Tough', // 作者简介
   LINK: 'https://itauge.blog', // 网站地址
-  KEYWORDS: 'Notion, Blog, japan', // 网站关键词 英文逗号隔开
+  KEYWORDS: 'Notion, Blog, Japan', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
@@ -42,10 +42,10 @@ const BLOG = {
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'mail@tangly1024.com',
+  CONTACT_EMAIL: '',
   CONTACT_WEIBO: '',
   CONTACT_TWITTER: '',
-  CONTACT_GITHUB: 'https://github.com/tangly1024',
+  CONTACT_GITHUB: 'https://github.com/itauge',
   CONTACT_TELEGRAM: '',
 
   // 鼠标点击烟花特效
